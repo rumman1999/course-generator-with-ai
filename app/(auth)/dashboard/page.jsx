1228c06a580a1a5004539dda0bcf84f7af52d75e
Rumman <rummanhase@gmail.com>
2024-09-01 01:15:06 +0530

    full authentiation setup along with logout and user setup

--- app/(auth)/dashboard/page.jsx
+++ app/(auth)/dashboard/page.jsx
@@ -1,8 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+        <UserButton/>
+    </div>
   )
 }
 
